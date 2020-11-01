b96393e09c3453a1911225943bb43a65b81fc666
desmoteuss <desmoteuss@gmail.com>
2020-11-01 16:05:47 +0100

    Project Completed

---
+++ WebScrapingProject.py
@@ -0,0 +1,103 @@
+from selenium import webdriver
+import openpyxl
+
+Path = "C:\chromedriver.exe"
+ExcelPath = "D:\\Project.xlsx"
+driver = webdriver.Chrome(Path)
+driver.get("https://rpa.hybrydoweit.pl")
+AriculeTitlesList= ['Tytul']
+AreaList= ['Branza']
+LinkList= ['Link']
+
+AriculeTitles = driver.find_elements_by_xpath("html/body/section/div/div/div/article/a")
+
+for AriculeTitle in AriculeTitles:
+    title = AriculeTitle.get_attribute('title')
+    AriculeTitlesList.append(title)
+
+
+areas = driver.find_elements_by_xpath("html/body/section/div/div/div/article/div/ul")
+
+for area in areas:
+       x = area.text
+       AreaList.append(x)
+
+
+links = driver.find_elements_by_xpath("html/body/section/div/div/div/article/a")
+
+
+for link in links:
+       href = link.get_attribute('href')
+       LinkList.append(href)
+      
+    
+driver.quit
+
+wb = openpyxl.Workbook()
+ws = wb.active
+
+
+for row in range(len(LinkList)):
+       ws.append([AriculeTitlesList[row],AreaList[row],LinkList[row]])
+
+
+wb.save(ExcelPath)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ws.append([links])
+
+# df = pd.DataFrame() 
+
+# df['link'] = links[0::2] 
+
+
+
+# for c in sheet['A']
+# sh.value=href
+
+
+# for r in range(1,max_row+1):
+#     for c in range(1,max_column+1):
+#         print(sh.cell(row=r, column=c).href)
+
+# for c in sheet['A']:
+#    print(c.value)
+
+
